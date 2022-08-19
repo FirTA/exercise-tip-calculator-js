918a515ca1b6e34c92853364cc9f2a7468e3e5bd
firdangid <firdauz.wokrspace@gmail.com>
2022-08-19 21:38:42 +0700

    updated script.js make function, +calculate bill tip and person, -format value to precision decimal

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 let numberOfPeople = 1
 
-const calculateBill = () => {
+// console.log(document.getElementsById("billTotalInput"))
 
+const getBillTotalValue = document.getElementById("billTotalInput")
+const getTipValue = document.getElementById("tipInput")
+
+
+const calculateBill = () => {
+    let billTotalValue = getBillTotalValue.value
+    let tipValue = getTipValue.value
+    console.log(billTotalValue +""+ tipValue)
+    let result = numberOfPeople*billTotalValue+((tipValue/100)*billTotalValue)
+    console.log("$" + result)
+    
+    document.
+    getElementById("perPersonTotal").innerText = result.toPrecision(3)
 }
 
+
 const increasePeople = () => {
+    let billInput = getBillTotalValue.value
+    console.log(billInput)
     numberOfPeople += 1
     console.log(numberOfPeople)
     document.getElementById("numberOfPeople").innerHTML = `${numberOfPeople}`
+    calculateBill()
     return numberOfPeople
 }
+
 
 const decreasePeople = () => {
     if (numberOfPeople > 1){
@@ -17,6 +35,7 @@ const decreasePeople = () => {
     }
     console.log(numberOfPeople)
     document.getElementById("numberOfPeople").innerHTML = `${numberOfPeople}`
+    calculateBill()
     return numberOfPeople
 }
 
