b84830b0b1a681797f389200386539015efecd26
firdangid <firdauz.wokrspace@gmail.com>
2022-08-19 17:06:11 +0700

    rev html, script.js create funciton for increase and decrease people

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-console.log("this is first project")
-console.log("nothing")
+let numberOfPeople = 1
+
+const calculateBill = () => {
+
+}
+
+const increasePeople = () => {
+    numberOfPeople += 1
+    console.log(numberOfPeople)
+    document.getElementById("numberOfPeople").innerHTML = `${numberOfPeople}`
+    return numberOfPeople
+}
+
+const decreasePeople = () => {
+    if (numberOfPeople > 1){
+        numberOfPeople -= 1
+    }
+    console.log(numberOfPeople)
+    document.getElementById("numberOfPeople").innerHTML = `${numberOfPeople}`
+    return numberOfPeople
+}
+
+// document.getElementById("numberOfPeople").innerHTML = `${numberOfPeople}`
