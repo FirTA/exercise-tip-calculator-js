386ea520010065723a0d4d704671925e1c1d2efb
firdangid <firdauz.wokrspace@gmail.com>
2022-08-20 06:24:07 +0700

    create recipt format in console log

--- script.js
+++ script.js
@@ -5,27 +5,66 @@ let numberOfPeople = 1
 const getBill = document.getElementById("billTotalInput")
 const getTip = document.getElementById("tipInput")
 
+// const purcahseRecipt = 
+// `   One Coffe Shop
+
+// Bill = ${bill}
+// `
+let recipt ={}
+
+const makeRecipt = (recipt) =>{
+    const takeRecipt ={
+        "billCurrent" : recipt.billCurrent,
+        "tipCurrent" : recipt.tipCurrent,
+        "numberOfPeople" : recipt.numberOfPeople
+    }
+
+    console.log(recipt.billCurrent)
+    const text = `
+    person = ${recipt.numberOfPeople}
+
+    bill
+        ........................$${takeRecipt.billCurrent}
+    tip
+        ........................$${takeRecipt.tipCurrent}
+                                ____+
+    total bill
+        ........................$${takeRecipt.billCurrent+takeRecipt.tipCurrent}
+
+    `
+
+    console.log(text)
+}
+
 
 const calculateBill = () => {
     let bill = Number(getBill.value)
-    let tip = Number(getTip.value)
+    let tip = Number(getTip.value)/100
     // console.log(typeof billTotalValue +  +""+ typeof tipValue)
-    console.log(bill.toPrecision(1))
-    let perPerson = (bill+((tip/100)*bill))/numberOfPeople
+    // console.log(bill.toPrecision(1))
+
+    let perPerson = (bill+(tip*bill))/numberOfPeople
     console.log("$" + typeof perPerson)
     
     // getBill.value = bill.toFixed(2)
     document.getElementById("perPersonTotal").innerText = `$${perPerson.toLocaleString('en-US')}`
+    recipt["billCurrent"] = bill
+    recipt["tipCurrent"] = tip
+    recipt["numberOfPeople"] = numberOfPeople
+    recipt["totalPerPerson"] = perPerson
+    console.log(recipt)
+    makeRecipt(recipt)
 }
 
 
 const increasePeople = () => {
     let billInput = getBill.value
-    console.log(billInput)
+    // console.log(billInput)
     numberOfPeople += 1
-    console.log(numberOfPeople)
+    // console.log(numberOfPeople)
     document.getElementById("numberOfPeople").innerHTML = `${numberOfPeople}`
     calculateBill()
+    // recipt["numberOfPeople"] = numberOfPeople
     return numberOfPeople
 }
 
@@ -37,7 +76,11 @@ const decreasePeople = () => {
     console.log(numberOfPeople)
     document.getElementById("numberOfPeople").innerHTML = `${numberOfPeople}`
     calculateBill()
+    // recipt["numberOfPeople"] = numberOfPeople
     return numberOfPeople
 }
+
+
+
 
 // document.getElementById("numberOfPeople").innerHTML = `${numberOfPeople}`
