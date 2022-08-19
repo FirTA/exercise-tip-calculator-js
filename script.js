8a8bd7344d55e9ba726b07d3f3085f4325d1cad4
firdangid <firdauz.wokrspace@gmail.com>
2022-08-19 21:57:27 +0700

    rev variable and function calculate result to devide per person

--- script.js
+++ script.js
@@ -2,24 +2,25 @@ let numberOfPeople = 1
 
 // console.log(document.getElementsById("billTotalInput"))
 
-const getBillTotalValue = document.getElementById("billTotalInput")
-const getTipValue = document.getElementById("tipInput")
+const getBill = document.getElementById("billTotalInput")
+const getTip = document.getElementById("tipInput")
 
 
 const calculateBill = () => {
-    let billTotalValue = getBillTotalValue.value
-    let tipValue = getTipValue.value
-    console.log(billTotalValue +""+ tipValue)
-    let result = numberOfPeople*billTotalValue+((tipValue/100)*billTotalValue)
-    console.log("$" + result)
+    let bill = Number(getBill.value)
+    let tip = Number(getTip.value)
+    // console.log(typeof billTotalValue +  +""+ typeof tipValue)
+    console.log(bill.toPrecision(1))
+    let perPerson = (bill+((tip/100)*bill))/numberOfPeople
+    console.log("$" + typeof perPerson)
     
-    document.
-    getElementById("perPersonTotal").innerText = result.toPrecision(3)
+    // getBill.value = bill.toPrecision(3)
+    document.getElementById("perPersonTotal").innerText = "$" + perPerson.toPrecision(3)
 }
 
 
 const increasePeople = () => {
-    let billInput = getBillTotalValue.value
+    let billInput = getBill.value
     console.log(billInput)
     numberOfPeople += 1
     console.log(numberOfPeople)
